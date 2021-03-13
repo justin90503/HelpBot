66e4f7c3d2358eecec79e1249f201aee0ffb1923
Justin Murphy <justin.murphy877@gmail.com>
2021-03-13 12:34:19 +0800

    ready for herokU

--- bot.js
+++ bot.js
@@ -13,28 +13,31 @@ client.on ("message", msg => {
     if (msg.content == '$ping') {
         msg.reply('Pong!');
     }
-    if(msg.content === "Embed") {
-        const embedmsg = new Discord.MessageEmbed()
-        .setColor('RED')
-        .setAuthor(`${msg.author.tag})`, msg.author.displayAvatarURL({ dynamic: true }))
-        .setTitle('Embed Is Fun')
-        .setDescription('I Have No Idea')
-        .addField('Field Title', 'Field Des')
-        .addField('Yeet', 'We go YEET!!!')
-        .addFields({ name: 'TITLE', value: 'Des'},
-                   { name: '/u2000', value: '**test** | __test__ | ~test~ | test' },
-                   { name: 'Inline Field', value: 'Inline Des', inline: true }
-                 )
-        .setTimestamp()
-        .setFooter('IM AMAZING')
-        msg.channel.send(embedmsg);     
-       }
+})
+
+client.on("message", msg => {
+if(msg.content === "Embed") {
+    const embedmsg = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setAuthor(`${msg.author.tag}`, msg.author.displayAvatarURL({ dynamic: true }))
+    .setTitle('This is a Test')
+    .setDescription('My first Embed Test')
+    .addField('Amazing', 'Cool')
+    .addField('Sus Noodles', 'Server')
+    .addFields({ name: 'Test', value: 'server'},
+               { name: 'Ive', value: 'Done Something' },
+               { name: 'Cool', value: 'fun', inline: true }
+             )
+    .setTimestamp()
+    .setFooter('IM AMAZING')
+    msg.channel.send(embedmsg);     
+   }
 });
 
 client.on("message", msg => {
     if (msg.content == "I love you") {
         msg.react("❤️")
     }
-})
+});
 
 client.login(process.env.BOT_TOKEN)
